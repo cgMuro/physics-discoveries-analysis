4796f3a9d7b45611e656da5605b3c2c1c549b591
Gio <cmurogio@gmail.com>
2020-09-08 14:20:56 +0200

    initial commit (data scraped, data cleaned, line charts, bar chart, hisotogram)

---
+++ scrape_data.py
@@ -0,0 +1,49 @@
+#https://en.wikipedia.org/wiki/Timeline_of_fundamental_physics_discoveries
+
+import requests
+import bs4
+import csv
+import re
+
+URL = 'https://steemit.com/physics/@danielcayee/timeline-of-fundamental-physics-discoveries'
+
+res = requests.get(URL)
+# Check for errors
+res.raise_for_status()
+
+# Create soup object
+soup = bs4.BeautifulSoup(res.content, 'html.parser')
+
+
+content = soup.find(class_='MarkdownViewer').next_element.next_element
+
+full_desc = content.text.split('\n')
+
+all_data = []
+
+for i in full_desc:
+    i = i.strip()
+    pattern = '([0-9]{4,4})|([0-9]{3,3} BCE)'
+    match = re.findall(pattern, i)
+    if match != []:
+        date = match[0][0] if match[0][0] != '' else match[0][1]
+    i = re.sub(pattern, '', i).strip()
+
+    if ':' in i:
+        # Get the person and the discovery
+        discovery, person = i.split(':')
+    else:
+        # Get the discovery if the person is not defined
+        person = 'NaN'
+        discovery = i
+    all_data.append((date.strip(), person.strip(), discovery.strip()))
+
+
+
+#Create csv file with the data
+with open('physics_discoveries.csv', 'w') as csv_file:
+    fieldnames = ['Date', 'Person', 'Discovery']
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+    for i in all_data: 
+        csv_writer.writerow({'Date': i[0], 'Person': i[1], 'Discovery': i[2]})
