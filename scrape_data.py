3103f8728621892eace0d7158d20505908b7d01e
Gio <cmurogio@gmail.com>
2020-09-11 15:52:04 +0200

    added comments

--- scrape_data.py
+++ scrape_data.py
@@ -1,12 +1,15 @@
-#https://en.wikipedia.org/wiki/Timeline_of_fundamental_physics_discoveries
+# WIKIPEDIA page with all the physics discoveries.
+# https://en.wikipedia.org/wiki/Timeline_of_fundamental_physics_discoveries
 
 import requests
 import bs4
 import csv
 import re
 
+# Website that I used to scrape the data. Because it was easier to clean.
 URL = 'https://steemit.com/physics/@danielcayee/timeline-of-fundamental-physics-discoveries'
 
+# Get request
 res = requests.get(URL)
 # Check for errors
 res.raise_for_status()
@@ -14,15 +17,17 @@ res.raise_for_status()
 # Create soup object
 soup = bs4.BeautifulSoup(res.content, 'html.parser')
 
-
+# Get the section of the page where all discoveries are listed
 content = soup.find(class_='MarkdownViewer').next_element.next_element
 
+# Create an array that cointains all the discoveries
 full_desc = content.text.split('\n')
 
 all_data = []
 
 for i in full_desc:
     i = i.strip()
+    # Get the data with regex
     pattern = '([0-9]{4,4})|([0-9]{3,3} BCE)'
     match = re.findall(pattern, i)
     if match != []:
@@ -37,7 +42,6 @@ for i in full_desc:
         person = 'NaN'
         discovery = i
     all_data.append((date.strip(), person.strip(), discovery.strip()))
-
 
 
 #Create csv file with the data
